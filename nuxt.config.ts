e21190b2812c58e2f542ca8c46464f1cce36c9ce
CrossDev <joancruz0502@hotmail.com>
2024-04-07 14:48:48 -0400

    feat: add vueuse and icons iconidy/carbon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/supabase', '@unocss/nuxt'],
+  modules: ['@nuxtjs/supabase', '@unocss/nuxt', '@vueuse/nuxt',],
   supabase: {
     url: process.env.SUPABASE_URL,
     key: process.env.SUPABASE_KEY,
