459fac94a1a99cc998a3de949b8ec228ad172dc5
CrossDev <joancruz0502@hotmail.com>
2024-04-07 17:32:03 -0400

    feat: add structure to login pages

--- src/components/ButtonPomo.vue
+++ src/components/ButtonPomo.vue
@@ -1,17 +1,15 @@
 <script setup lang="ts">
 import Button from 'primevue/button';
-
-const { icon, label, color} = defineProps<{
+defineProps<{
     icon: string;
     label: string;
-    color: string;
+
 }>();
 
-const style = `color-${ color } border-${ color } hover:bg-${ color }`
 
 </script>
 <template>
-    <Button m-2 p-2 flex justify-center items-center gap-1 hover:text-white :class="style">
+    <Button m-2 p-2 flex justify-center items-center gap-1 hover:text-white bg-mountain-50>
         <div  :class="icon"></div>
         <span>{{ label }}</span>
     </Button>
--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,25 +1,24 @@
 <script setup lang="ts">
-const supabase = useSupabaseClient()
-const email = ref('')
+import useAuthSupabase from '~/composables/useAuthSupabase';
+import Button from 'primevue/button';
+import ButtonPomo from '~/components/ButtonPomo.vue';
 
-const signInWithOtp = async () => {
-  const { error } = await supabase.auth.signInWithOtp({
-    email: email.value,
-    options: {
-      emailRedirectTo: 'http://localhost:3000/confirm',
-    }
-  })
-  if (error) console.log(error)
-}
+const { signInWithOAuth } = useAuthSupabase();
+
 </script>
+
 <template>
-  <div>
-    <button @click="signInWithOtp">
-      Sign In with E-Mail
-    </button>
-    <input
-      v-model="email"
-      type="email"
-    />
+  <div w-full h-full flex justify-center items-center>
+    <div h-70 w-[700px] flex="~ col" border="~ black" rounded-md justify-around items-center mt-10 my-4 p-2>
+      <div>
+        <h3 text-5xl font-ubuntu font-bold>SignIn</h3>
+        <p font-regular text-gray>To continue</p>
+      </div>
+      <div flex justify-center items-center gap-2>
+        <ButtonPomo label="Sign in with Github" icon="i-carbon-logo-github" color-black border-black hover:bg-black @click="signInWithOAuth('github')"/>
+        <ButtonPomo label="Sign in with Google" icon="i-carbon-logo-google" color-coral border-coral hover:bg-coral @click="signInWithOAuth('google')"/>
+        <ButtonPomo label="Sign in with Discord" icon="i-carbon-logo-discord" color-purple-900 border-purple-900 hover:bg-purple-900 @click="signInWithOAuth('discord')"/>
+      </div>
+    </div>
   </div>
 </template>
