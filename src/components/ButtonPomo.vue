043aeb3d7537e806838e2e68243fdb018227775c
CrossDev <joancruz0502@hotmail.com>
2024-04-07 17:31:31 -0400

    feat: created custom button

---
+++ src/components/ButtonPomo.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import Button from 'primevue/button';
+
+const { icon, label, color} = defineProps<{
+    icon: string;
+    label: string;
+    color: string;
+}>();
+
+const style = `color-${ color } border-${ color } hover:bg-${ color }`
+
+</script>
+<template>
+    <Button m-2 p-2 flex justify-center items-center gap-1 hover:text-white :class="style">
+        <div  :class="icon"></div>
+        <span>{{ label }}</span>
+    </Button>
+</template>
