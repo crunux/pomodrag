1162ffc2f26936a9efb52b28fad7d663cfa44135
CrossDev <joancruz0502@hotmail.com>
2024-04-07 14:47:53 -0400

    feat: add theme colors unocss

--- uno.config.ts
+++ uno.config.ts
@@ -1,5 +1,5 @@
 import {
-    defineConfig, 
+    defineConfig,
     presetAttributify,
     presetIcons,
     presetTypography,
@@ -8,7 +8,23 @@ import {
 
 export default defineConfig({
     theme: {
-        colors: {}
+        colors: {
+            'mountain': {
+                '50': '#ebfef7',
+                '100': '#d0fbe8',
+                '200': '#a4f6d7',
+                '300': '#6aebc2',
+                '400': '#2ed9a9',
+                '500': '#0abd90',
+                '600': '#009b77',
+                '700': '#007c63',
+                '800': '#03624f',
+                '900': '#035142',
+                '950': '#012d27',
+            },
+            'golden': '#eda613',
+            'coral': '#ec4646',
+        }
     },
     presets: [
         presetUno(),
