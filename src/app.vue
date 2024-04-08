ef4d06c74747ce9f6835326b37f2854ce3eb4e9f
CrossDev <joancruz0502@hotmail.com>
2024-04-08 00:07:51 -0400

    feat: add width and height style

--- src/app.vue
+++ src/app.vue
@@ -1,5 +1,23 @@
 <template>
-  <div>
-    <NuxtPage/>
+  <div class="flex flex-col justify-between items w-10/12 md:w-10/12 lg:w-9/12 xl:w-7/12 h-[calc(100svh-1.25rem)]">
+    <NuxtLayout>
+      <NuxtPage/>
+    </NuxtLayout>
   </div>
 </template>
+<style>
+.dark body {
+  background: #303231;
+  color: #fafafa;
+}
+
+.light body {
+  background: #fafafa;
+  color: #303231;
+}
+
+body {
+    min-height: 100vh;
+    margin: 0 auto;
+}
+</style>
