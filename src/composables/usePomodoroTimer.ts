dd390b03a2519f02d8587170f0b87803d3aa0d63
CrossDev <joancruz0502@hotmail.com>
2024-04-14 12:16:36 -0400

    chore: changed name type

--- src/composables/usePomodoroTimer.ts
+++ src/composables/usePomodoroTimer.ts
@@ -1,18 +1,19 @@
 import type { Timeout } from 'node'
 
-interface Timer {
+interface PomodoroTimer {
     name: string
     duration: number
 }
+
 
 const usePomodoroTimer = () => {
 
     const isRunning = ref<boolean>(false)
     const isPaused = ref<boolean>(false)
-    const timerInstance = ref<Timeout>()
+    const timerInstance = ref<Timeout>({})
     const totalSeconds = ref<number>(25 * 60)
     const currentTimer = ref<number>(0)
-    const timers = reactive<Timer[]>([{ name: 'Pomodoro', duration: 25 * 60 }, { name: 'Short Break', duration: 5 * 60 }, { name: 'Long Break', duration: 15 * 60 }])
+    const timers = reactive<PomodoroTimer[]>([{ name: 'Pomodoro', duration: 25 * 60 }, { name: 'Short Break', duration: 5 * 60 }, { name: 'Long Break', duration: 15 * 60 }])
     const displayMinute = computed(() => {
         const minutes = Math.floor(totalSeconds.value / 60)
         return formatTime(minutes)
@@ -74,6 +75,8 @@ const usePomodoroTimer = () => {
         currentTimer,
         // expose the following computed
         timer: computed(() => `${displayMinute.value}:${displaySecond.value}`),
+        minute: displayMinute,
+        second: displaySecond,
         // expose the following methods
         start,
         pause,
