0a2af6d388d8bb2ca699e1f2a2bf42d6247de076
CrossDev <joancruz0502@hotmail.com>
2024-04-14 17:36:50 -0400

    chore: refactor fecth chores

--- src/composables/useFetchChores.ts
+++ src/composables/useFetchChores.ts
@@ -1,23 +1,51 @@
-import type { Chore } from '@/types/index'
+import type { Chore, DragDropChore } from '@/types/index'
 import type { Database } from '~/types/database.types';
 
+interface FecthChores {
+    chores: Chore[]
+    error: any
 
-const useGetChoresSupabase = async () => {
+}
+
+interface useFetchReturn {
+    choresNew: Chore[];
+    choresInProcess: Chore[];
+    choresDone: Chore[];
+    chores: DragDropChore[];
+    error: any;
+}
+
+
+const useGetChoresSupabase = async (): Promise<useFetchReturn | undefined> => {
     const user = useSupabaseUser();
+    const chores = ref<Chore[] | undefined>([])
+    const error = ref<any>()
     if (!user) return
     try {
-        const { data } = await useFetch<Chore[]>('/api/chores', {
+        const { data } = await useFetch<FecthChores>('/api/chores', {
             method: 'GET',
         });
-
-        return {
-            chores: data.value as Chore[]
+        if (data?.value?.chores) {
+            chores.value = data?.value?.chores
+        }
+        if (data?.value?.error) {
+            error.value = data?.value?.error
         }
     }
     catch (error) {
-        return {
-            error
-        }
+        console.error(error);
+    }
+
+    return {
+        choresNew: chores.value?.filter(chore => chore.status === 'new') as Chore[] || [],
+        choresInProcess: chores.value?.filter(chore => chore.status === 'in-process') as Chore[] || [],
+        choresDone: chores.value?.filter(chore => chore.status === 'done') as Chore[] || [],
+        chores: [
+            { id: 1, title: 'New Chores', status: 'new', chores: chores.value?.filter(chore => chore.status === 'new') as Chore[] || [] },
+            { id: 2, title: 'In-Process Chores', status: 'in-process', chores: chores.value?.filter(chore => chore.status === 'in-process') as Chore[] || [] },
+            { id: 3, title: 'Done Chores', status: 'done', chores: chores.value?.filter(chore => chore.status === 'done') as Chore[] || [] },
+        ] as DragDropChore[],
+        error: error.value || undefined,
     }
 
 }
