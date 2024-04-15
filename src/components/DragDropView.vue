b2728d44d1e67a14c44a0506d7526660e5b1a516
CrossDev <joancruz0502@hotmail.com>
2024-04-15 00:28:12 -0400

    chore: create input and viewdragdrop, divided by components

---
+++ src/components/DragDropView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="md:w-[45%] min-w-[278px]" h-full py-2 m-2 flex="~ col" justify-center items-start pl-0 lg:pl-10 rounded-md>
+        <InputCreate/>
+        <ChoreBoard/>
+    </div>
+</template>
---
+++ src/components/InputCreate.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import type { ChoreCreate } from '@/types'; // Add this import statement
+const { createChore } = useCreateChore()
+const user = useSupabaseUser()
+
+const chore = reactive<ChoreCreate>({
+    content: '',
+    status: 'new'
+})
+
+</script>
+<template>
+  <div flex gap-2 items-start>
+        <input border="~ mountain-400 focus:active:hover:mountain-900" w-40 md:w-auto text-black:70 rounded-md id="chore" v-model="chore.content" aria-describedby="create-chore" @click.enter="createChore({content: chore.content, status: chore.status})"/>
+        <p font-bold dark:color-white color-mountain-900 aria-describedby="title-chore-seccion">Chores</p>
+        <small color-gray-500 text-xs v-show="!user" id="chores-info" aria-describedby="chores-info">Only available with login.</small>
+      </div>
+</template>
+<style scoped></style>
