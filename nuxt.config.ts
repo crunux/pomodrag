2a48abdf1e7b96cc6d673d70a654035b23e27cf4
CrossDev <joancruz0502@hotmail.com>
2024-04-16 22:53:42 -0400

    chore: remove link SEO put in app

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,11 +3,6 @@ export default defineNuxtConfig({
   devtools: { enabled: false },
   srcDir: 'src',
   modules: ['@nuxtjs/supabase', '@unocss/nuxt', '@vueuse/nuxt', "@nuxt/image", "@nuxtjs/color-mode"],
-  app:{
-    head: {
-      link: [{ rel: 'icon', type: 'image/png', href: '/favicon.png' }],
-    }
-  },
   supabase: {
     url: process.env.SUPABASE_URL,
     key: process.env.SUPABASE_KEY,
