67dc9855abe28abc6bb1ce25f682b0d7ede8b0d2
CrossDev <joancruz0502@hotmail.com>
2024-04-16 21:59:03 -0400

    chore: refactor update chore dimanyc

--- src/server/api/chores.put.ts
+++ src/server/api/chores.put.ts
@@ -3,13 +3,14 @@ import type { Chore } from '~/types'
 import type { Database } from '~/types/database.types'
 
 export default defineEventHandler(async (event) => {
+    const { field, chore } = await readBody(event)
+    console.log(field);
     const user = await serverSupabaseUser(event)
     const client = await serverSupabaseClient<Database>(event)
-    const { chore } = await readBody(event)
-    if (!user) return
-    const { data: chores, error } = await client.from('chores').update(chore).eq('userId', user.id).returns<Chore[]>().then(response => response)
+    if (!user || !field) return
+    const { data, error } = await client.from('chores').update({ [`${field}`]: chore[`${field}`] }).eq('id', chore.id).returns<Chore>()
     return {
-        chores: chores as Array<Chore>,
+        chore: data as Chore,
         error
     }
 })
