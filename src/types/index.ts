7655f7dd1fd93904f1cd63c8257171d8a56338d7
CrossDev <joancruz0502@hotmail.com>
2024-04-28 02:37:09 -0400

    chore: add type for alarm timer

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,4 @@
+import type { Howl, HowlOptions } from 'howler'
 export type ProviderOAuth = 'github' | 'google' | 'discord';
 
 export type StateChore = 'new' | 'in-process' | 'done';
@@ -24,4 +25,42 @@ export interface DragEvent extends Event {
     added: { element: Chore } | undefined
     removed: { element: Chore } | undefined
     moved: { element: Chore } | undefined
+}
+
+
+export interface HowlStatic {
+  new (properties: HowlOptions): Howl
+}
+
+export type MaybeRef<T> = T | Ref<T> | ComputedRef<T>
+
+export interface SpriteMap {
+  [key: string]: [number, number]
+}
+
+export type ComposableOptions = {
+  volume?: MaybeRef<number>
+  playbackRate?: MaybeRef<number>
+  interrupt?: boolean
+  soundEnabled?: boolean
+  autoplay?: boolean
+  sprite?: SpriteMap
+  onload?: () => void
+} & Omit<HowlOptions, 'src'>
+
+export interface PlayOptions {
+  id?: number
+  forceSoundEnabled?: boolean
+  playbackRate?: number
+}
+
+export type PlayFunction = (options?: PlayOptions) => void
+
+export interface ReturnedValue {
+  play: PlayFunction
+  sound: Ref<Howl | null>
+  stop: (id?: number) => void
+  pause: (id?: number) => void
+  isPlaying: Ref<boolean>
+  duration: Ref<number | null>
 }
