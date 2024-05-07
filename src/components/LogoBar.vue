414c4cd01ff478918065802e42d965f2ca987997
CrossDev <joancruz0502@hotmail.com>
2024-05-07 18:28:42 -0400

    chore: add logo in the navbar

--- src/components/LogoBar.vue
+++ src/components/LogoBar.vue
@@ -1,13 +1,12 @@
 <script setup lang="ts">
-const colorMode = useColorMode();
+const { value } = useColorMode();
 </script>
 <template>
     <NuxtLink to="/">
         <NuxtImg w-40
             md:w-60
             lg:w-82
-            :src="colorMode.value === 'dark'?'/images/logodark.png': '/images/logolight.png'"
+            :src="value === 'dark' ? '/images/logodark.png' : '/images/logolight.png'"
             alt="pomodrag logo" />
     </NuxtLink>
-
 </template>
