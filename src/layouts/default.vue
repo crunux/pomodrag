0cca5df9643f6de0fdafc4fccba22ef443d82dde
CrossDev <joancruz0502@hotmail.com>
2024-05-10 19:28:06 -0400

    chore: update dependency

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,12 +1,5 @@
-<script setup lang="ts">
-// const logoutUser = () => {
-//   signOut()
-// }
-
-</script>
 <template>
   <NavBar />
   <slot />
   <FooterBottom />
 </template>
-<style scoped></style>
