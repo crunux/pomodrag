32c86f21799312e788c2a3b97c9b2077296d86b6
CrossDev <joancruz0502@hotmail.com>
2024-05-10 18:45:30 -0400

    feat: create modal component

---
+++ src/components/DialogModal.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="inline-block [--trigger:click] [--placement:bottom] group">
+    <slot name="button">
+      <span
+        class="group-hover:opacity-100 group-hover:visible opacity-0 transition-opacity inline-block absolute invisible z-10 py-3 px-4 bg-white border text-sm text-gray-600 rounded-lg shadow-md dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400"
+        role="tooltip">
+        <slot name="content" />
+      </span>
+    </slot>
+  </div>
+</template>
+<style scoped></style>
